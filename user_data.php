6873e47ec6c8825033ad677180261f2c1dead458
Joseph O <on.josephinfo@gmail.com>
2022-10-19 23:57:14 +0100

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -1,26 +1,22 @@
 <?php
-print_r($_POST);
-$name = $_POST['name'];
-$email = $_POST['email'];
-$DOB = $_POST['DOB'];
-$gender = $_POST['gender'];
-$country = $_POST['country'];
+if(isset($_POST['btnRegister']) && $_SERVER['REQUEST_METHOD'] == "POST"){
+    if(isset($_POST['name'])) $name = trim($_POST['name']); else echo "Name is required!";
+    if(isset($_POST['email'])) $email = trim($_POST['email']); else echo "email is required!";
+    if(isset($_POST['dob'])) $dob = trim($_POST['dob']); else echo "dob is required!";
+    if(isset($_POST['gender'])) $gender = trim($_POST['gender']); else echo "gender is required!";
+    if(isset($_POST['country'])) $country = trim($_POST['country']); else echo "country is required!";
 
-echo "NAME: " . $name . "<br>";
-echo "EMAIL: " . $email . "<br>";
-echo "DOB: " . $DOB . "<br>";
-echo "gender: " . $gender . "<br>";
-echo "country: " . $country . "<br>";
+    $filename = "./userdata.csv";
+    $handle = fopen($filename, 'w');
+    $data = array(
+        array('Name', 'Email', 'Date of Birth', 'Gender', 'Country'),
+        array($name, $email, $dob, $gender, $country)
+    );
 
-$csv = 
-array($_POST['name'],$_POST['email'],$_POST['DOB'],$_POST['country']);
-
-$file = fopen('form.csv', 'a');
-
-foreach ($csv as $line) 
-{
-  fputcsv($file,explode(',',$line));
+    foreach ($data as $line){
+        if(fputcsv($handle, $line)){
+            print_r($line);
+        } else echo "Unable to write to file!";
+    }
+    fclose($handle);
 }
-
-fclose($file); 
-?>
